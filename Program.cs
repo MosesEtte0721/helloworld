a02d1fb47563fadb29bc165cf804283ff0bc3d30
Moses Ette <mette@byupathway.edu>
2026-01-17 10:50:27 +0100

    Comment out other lines of code that are not 'hello world'

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello World!");
-Console.WriteLine("Welcome to your first C# program.");
+Console.WriteLine("Welcome to my first C# program.");
 
-String firstName = "   Chris   ";
-String lastName = "    Utom ";
+// String firstName = "   Chris   ";
+// String lastName = "    Utom ";
 
-Console.WriteLine($"His name is {firstName.Trim()} {lastName.TrimStart()}");
-Console.WriteLine($"His name is {firstName} {lastName}");
+// Console.WriteLine($"His name is {firstName.Trim()} {lastName.TrimStart()}");
+// Console.WriteLine($"His name is {firstName} {lastName}");
 
-firstName = firstName.Trim();
-lastName = lastName.TrimStart();
+// firstName = firstName.Trim();
+// lastName = lastName.TrimStart();
 
-Console.WriteLine($"His name is {firstName} {lastName}");
-String name = $"{firstName} {lastName}";
+// Console.WriteLine($"His name is {firstName} {lastName}");
+// String name = $"{firstName} {lastName}";
 
-Console.WriteLine(name.Contains("Ette"));
-Console.WriteLine(name.Contains("Chris"));
+// Console.WriteLine(name.Contains("Ette"));
+// Console.WriteLine(name.Contains("Chris"));
 
-Console.WriteLine(name.Replace("Chris", "Bestie"));
-Console.WriteLine(name.Contains("Chris"));
-name = name.Replace("Chris", "Bestie");
-Console.WriteLine(name.Contains("Chris"));
+// Console.WriteLine(name.Replace("Chris", "Bestie"));
+// Console.WriteLine(name.Contains("Chris"));
+// name = name.Replace("Chris", "Bestie");
+// Console.WriteLine(name.Contains("Chris"));
 
