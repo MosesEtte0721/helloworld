80b21f5dd69f015f3774be02da6525e61a0fafcc
Moses Ette <mette@byupathway.edu>
2026-01-17 10:11:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World!");
+Console.WriteLine("Welcome to your first C# program.");
+
+String firstName = "   Chris   ";
+String lastName = "    Utom ";
+
+Console.WriteLine($"His name is {firstName.Trim()} {lastName.TrimStart()}");
+Console.WriteLine($"His name is {firstName} {lastName}");
+
+firstName = firstName.Trim();
+lastName = lastName.TrimStart();
+
+Console.WriteLine($"His name is {firstName} {lastName}");
+String name = $"{firstName} {lastName}";
+
+Console.WriteLine(name.Contains("Ette"));
+Console.WriteLine(name.Contains("Chris"));
+
+Console.WriteLine(name.Replace("Chris", "Bestie"));
+Console.WriteLine(name.Contains("Chris"));
+name = name.Replace("Chris", "Bestie");
+Console.WriteLine(name.Contains("Chris"));
+
